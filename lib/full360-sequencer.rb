af239f1c9aac1f83cad16e6153cd53ba88f36184
Jeremy Winters <jeremy.winters@full360.com>
2017-06-22 18:10:48 -0700

    0.0.2

---
+++ lib/full360-sequencer.rb
@@ -0,0 +1,146 @@
+#!/usr/bin/ruby
+require 'aws-sdk'
+require 'yaml'
+require 'logger'
+require 'pp'
+
+module Full360
+  module Sequencer
+    class Runner
+      attr_accessor :sleep_between_checks
+      attr_accessor :config
+      
+      def initialize(logger = nil)
+        @logger = logger ? logger : Logger.new(STDOUT)
+
+        # default 5 seconds between completed? checks
+        @sleep_between_checks = 5
+      end
+      
+      def config_from_file(yaml_path)
+        @config = parse_config_file(yaml_path)
+      end
+      
+      def run_task_class(task_type_string)
+        case task_type_string
+        when 'ecs_task' then Full360::Sequencer::RunECSTask
+        else nil
+        end
+      end
+
+      def run
+        @config.each do |params|
+          this_task_name = task_name(params)
+          this_task = run_task_class(params[this_task_name]['type']).new(
+            this_task_name,
+            params[this_task_name]
+          )
+          this_task.run_task
+          until this_task.completed?
+            sleep @sleep_between_checks
+          end
+          raise "task failed error" unless this_task.success
+        end
+      rescue => e
+        @logger.error(e.message)
+        e.backtrace.each { |r| @logger.error(r) }
+      end
+      
+      def task_name(params)
+        params.keys.first
+      end
+      
+      def parse_config_file(yaml_path)
+        YAML.load_file(yaml_path)
+      end
+      
+      def config_valid?(config)
+        return false unless config.is_a? Array
+        true
+      end
+    end
+    
+    class RunTaskBase
+      attr_reader :success
+      attr_reader :exit_code
+      
+      def run_task; end
+      def completed?; end
+      def kill_task; end #will be used for timeout
+    end
+
+    class RunECSTask < RunTaskBase
+      def initialize(task_name, params, logger = nil)
+        @logger = logger ? logger : Logger.new(STDOUT)
+        @task_name = task_name
+        @params = params['parameters']
+        @params = keys_to_symbol(@params)
+        @cluster = @params[:cluster]
+      end
+      
+      def keys_to_symbol(params)
+        # replaces string keys with symbol keys
+        # required by AWS SDK
+        if params.is_a?(Hash)
+          params.inject({}){ |memo,(k,v)| memo[k.to_sym] = v; memo }
+        else
+          nil
+        end
+      end
+
+      def run_task
+        @logger.info("starting ECS task #{@task_name}")
+        @logger.debug("creating AWS client for ECS task #{@task_name}...")
+        @ecs_client = ::Aws::ECS::Client.new
+        @logger.debug("running ECS task #{@task_name}...")
+        @start_time = Time.new
+        resp = @ecs_client.run_task(@params)
+        @task_arn = resp.tasks[0].task_arn
+        @logger.info("#{@task_name} task created #{@task_arn} on cluster #{@cluster}")
+      end
+
+      def ecs_describe_tasks
+        @ecs_client.describe_tasks(
+          {
+            cluster: @cluster,
+            tasks: [@task_arn] # required
+          }
+        )
+      end
+      
+      def completed?
+        resp = ecs_describe_tasks
+        status = last_task_status(resp)
+        @logger.info("#{@task_name} : #{@task_arn} current status: #{status}")
+        if status == 'STOPPED'
+          @logger.info("#{@task_name} completed in #{Time.new - @start_time} seconds")
+          # parse exit_code(s) and return completion
+          @success = determine_success(resp)
+          return true 
+        end
+        false
+      rescue => e
+        @logger.error(e.message)
+        e.backtrace.each { |r| @logger.error(r) }
+      end
+      
+      # parses last status from aws API response
+      def last_task_status(resp)
+        resp.tasks[0].last_status
+      end
+      
+      # success is determined by all containers having zero exit code
+      def determine_success(resp)
+        success = true
+        resp.tasks[0].containers.each do |c|
+          @logger.info("#{@task_name} : container #{c.name} #{c.container_arn} completed with exit_code #{c.exit_code}")
+          if c.exit_code != 0
+            # we had a problem!
+            success = false
+          end
+        end
+        success
+      end
+    end
+  end
+end
---
+++ test/sequencer_test.rb
@@ -0,0 +1,103 @@
+gem 'minitest'
+require 'minitest/autorun'
+
+# using a global variable because this is only a test
+$base_path = File.expand_path("#{File.dirname(__FILE__)}/../")
+
+require_relative "#{$base_path}/lib/full360-sequencer.rb"
+
+class TestSequencerRunner < Minitest::Test
+  def create_runner
+    Full360::Sequencer::Runner.new
+  end
+
+  def valid_yml_as_object
+    [{"chicken_butter"=>
+     {"type"=>"ecs_task",
+      "ignore_failure"=>false,
+      "active"=>true,
+      "parameters"=>
+       {"cluster"=>"yourcluster",
+        "task_definition"=>"test-task-def:1",
+        "count"=>1}}},
+   {"turkey_fingers"=>
+     {"type"=>"ecs_task",
+      "ignore_failure"=>false,
+      "active"=>true,
+      "parameters"=>
+       {"cluster"=>"yourcluster2",
+        "task_definition"=>"test-task-def:2",
+        "count"=>1}}}]
+  end
+
+  def test_run_task_class
+    f = create_runner
+
+    assert_equal(
+      Full360::Sequencer::RunECSTask,
+      f.run_task_class('ecs_task')
+    )
+
+    assert_equal(
+      nil,
+      f.run_task_class('bad_info')
+    )
+  end
+
+  def test_task_name
+    f = create_runner
+
+    assert_equal(
+      'chicken',
+      f.task_name({'chicken' => 2, 'turkey' => 3})
+    )
+  end
+
+  def test_parse_config_file
+    f = create_runner
+
+    assert_equal(
+      valid_yml_as_object,
+      f.parse_config_file("#{$base_path}/test/fixtures/valid.yml")
+    )
+  end
+
+  def test_config_valid?
+    f = create_runner
+
+    assert_equal(
+      true,
+      f.config_valid?([1, 2, 3])
+    )
+
+    assert_equal(
+      false,
+      f.config_valid?({a: 1})
+    )
+  end
+
+  def test_config_from_file
+    f = create_runner
+    f.config_from_file("#{$base_path}/test/fixtures/valid.yml")
+    assert_equal(
+      valid_yml_as_object,
+      f.config
+    )
+  end
+end
+
+class TestRunECSTask < Minitest::Test
+  def test_keys_to_symbol
+    t = Full360::Sequencer::RunECSTask.new(
+      'test-task',
+      {'parameters' => {}}
+    )
+
+    assert_equal(
+      {a: 1, b: 2},
+      t.keys_to_symbol(
+        {'a' => 1, 'b' => 2}
+      )
+    )
+  end
+end
