9870b656310bb2bf22930114027ed2ad5dd872ea
Alberto Grespan <alberto@albertogrespan.com>
2021-09-30 14:59:54 -0400

    Cleanup the tests after the symbolize change

--- test/sequencer_test.rb
+++ test/sequencer_test.rb
@@ -1,9 +1,9 @@
+require "pry"
 require "minitest/autorun"
+require "full360-sequencer"
 
 # using a global variable because this is only a test
 $base_path = File.expand_path("#{File.dirname(__FILE__)}/../")
-
-require_relative "#{$base_path}/lib/full360-sequencer.rb"
 
 class TestSequencerRunner < Minitest::Test
   def create_runner
@@ -11,22 +11,33 @@ class TestSequencerRunner < Minitest::Test
   end
 
   def valid_yml_as_object
-    [{"chicken_butter"=>
-     {"type"=>"ecs_task",
-      "ignore_failure"=>false,
-      "active"=>true,
-      "parameters"=>
-       {"cluster"=>"yourcluster",
-        "task_definition"=>"test-task-def:1",
-        "count"=>1}}},
-   {"turkey_fingers"=>
-     {"type"=>"ecs_task",
-      "ignore_failure"=>false,
-      "active"=>true,
-      "parameters"=>
-       {"cluster"=>"yourcluster2",
-        "task_definition"=>"test-task-def:2",
-        "count"=>1}}}]
+    [
+      {
+        chicken_butter: {
+          type: "ecs_task",
+          ignore_failure: false,
+          active: true,
+          parameters: {
+            cluster: "yourcluster",
+            task_definition: "test-task-def:1",
+            count: 1
+          },
+        }
+      },
+      {
+        turkey_fingers: {
+          type: "ecs_task",
+          ignore_failure: false,
+          active: true,
+          parameters: {
+            cluster: "yourcluster2",
+            task_definition: "test-task-def:2",
+            count: 1,
+            launch_type: "FARGATE"
+          },
+        }
+      }
+    ]
   end
 
   def test_run_task_class
@@ -47,17 +58,17 @@ class TestSequencerRunner < Minitest::Test
     f = create_runner
 
     assert_equal(
-      "chicken",
-      f.task_name({"chicken" => 2, "turkey" => 3})
+      :chicken,
+      f.task_name({ chicken: 2, turkey: 3 })
     )
   end
 
-  def test_parse_config_file
+  def test_config_from_file
     f = create_runner
 
     assert_equal(
       valid_yml_as_object,
-      f.parse_config_file("#{$base_path}/test/fixtures/valid.yml")
+      f.config_from_file("#{$base_path}/test/fixtures/valid.yml")
     )
   end
 
@@ -77,26 +88,11 @@ class TestSequencerRunner < Minitest::Test
 
   def test_config_from_file
     f = create_runner
+
     f.config_from_file("#{$base_path}/test/fixtures/valid.yml")
     assert_equal(
       valid_yml_as_object,
       f.config
-    )
-  end
-end
-
-class TestRunECSTask < Minitest::Test
-  def test_keys_to_symbol
-    t = Full360::Sequencer::RunECSTask.new(
-      "test-task",
-      {"parameters" => {}}
-    )
-
-    assert_equal(
-      {a: 1, b: 2},
-      t.keys_to_symbol(
-        {"a" => 1, "b" => 2}
-      )
     )
   end
 end
