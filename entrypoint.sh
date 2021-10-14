d566b7f337ed9754557f0ccb4d3a9741f380be60
Alberto Grespan <alberto@albertogrespan.com>
2021-10-14 18:38:23 -0400

    Download the YAML file from S3 using Ruby
    Instead of using the AWS SDK to download a file use pure Ruby to
    accomplish this as everything should work without setting the profile
    for the role.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-if [ -n "$AWS_REGION" ]; then
-  aws s3 cp $SEQUENCER_YAML_S3_PATH sequencer.yml --region $AWS_REGION
-else
-  aws s3 cp $SEQUENCER_YAML_S3_PATH sequencer.yml
-fi
-
-sequencer sequencer.yml
+sequencer $SEQUENCER_YAML_S3_PATH
